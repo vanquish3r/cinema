f0e79f27387c75e6100f792723ea39bef0d76373
Vanquish3r <153280214+vanquish3r@users.noreply.github.com>
2026-01-14 18:27:34 -0700

    Updated FireScreen Scale

--- bullshcript.js
+++ bullshcript.js
@@ -95,7 +95,7 @@ function enableTheFireScreen() {
 			screenstuffDisabled = false;
 			console.log("Adding Screen Cast");
 			const firescreenAttributes = {
-				"scale": "0.7 0.7 1",
+				"scale": "0.6 0.6 1",
 				"mipmaps": "0",
 				"rotation": "0 0 0",
 				"screen-rotation": "0 0 0",
